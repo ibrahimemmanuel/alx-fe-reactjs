4bc846c77959ed5bbbfe766713708548cadd0f6a
Ibrahim Emmanuel <emmanuelibrahim862@gmail.com>
2025-07-31 08:34:24 +0100

    New Updates

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile'; // 👈 this line MUST exist exactly
 
 function App() {
   return (
     <>
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
   );
