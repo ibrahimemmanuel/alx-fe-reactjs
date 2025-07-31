541798b01eb19d9b026ab8cdf350506544cfec41
Ibrahim Emmanuel <emmanuelibrahim862@gmail.com>
2025-07-31 10:08:32 +0100

    new push

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -4,7 +4,7 @@ function MainContent() {
   return (
     <main style={{ padding: '1rem', backgroundColor: '#0035bcff' }}>
       <p style={{ fontSize: '1.2rem' }}>
-        Welcome to the list of some of my favorite cities around the world!
+        Welcome to the list of some of my favorite cities around the world! "I love to visit New York, Paris, and Tokyo."]
       </p>
     </main>
   );
