f96f1728599556946a9f8f3e7cf47627dd57b30e
Ibrahim Emmanuel <emmanuelibrahim862@gmail.com>
2025-07-31 07:44:44 +0100

    Moved WelcomeMessage.jsx to src folder and removed components folder

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import WelcomeMessage from './WelcomeMessage';
 import MainContent from './MainContent';
 import Footer from './Footer';
 
