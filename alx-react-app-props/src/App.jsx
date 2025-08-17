878db5250f26e472a856d5dec8686a6be0b8a7d3
Ibrahim Emmanuel <emmanuelibrahim862@gmail.com>
2025-08-17 13:26:55 +0100

    Update alx-react-app-props project

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,8 +3,8 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile'; // 👈 this line MUST exist exactly
 // src/App.jsx
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import ProfilePage from "./ProfilePage";
+import UserContext from "./UserContext";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
