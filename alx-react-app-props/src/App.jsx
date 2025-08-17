a3ceda6b9ffb136d1c9a9b57bc5e08e10934390e
Ibrahim Emmanuel <emmanuelibrahim862@gmail.com>
2025-08-17 13:11:55 +0100

    Add alx-react-app-props project

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,21 @@
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile'; // 👈 this line MUST exist exactly
+// src/App.jsx
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
+
+
